3d41491214dd5d0941417efe398cf59f0adf66f5
leticialopes1-gif <leticia.lopes1@estudante.ufla.br>
2026-06-11 17:33:12 -0300

    Delete EntidadeAluno class and its methods
    Removed the EntidadeAluno class and its related methods.

--- src/entities/EntidadeAluno.java
+++ src/entities/EntidadeAluno.java
@@ -73,39 +73,5 @@ public class EntidadeAluno extends EntidadeUsuario {
                 aluno.getMatricula()
         );
     }
-}package entities;
-
-import domain.Aluno;
-import domain.Usuario;
-
-public class EntidadeAluno extends EntidadeUsuario {
-    private String matricula;
-
-    public EntidadeAluno() {}
-
-    protected EntidadeAluno(String nome, String cpf, int emprestimosAtivos, String matricula) {
-        super(nome, cpf, emprestimosAtivos);
-        this.matricula = matricula;
-    }
-
-    public String getMatricula() {
-        return matricula;
-    }
-
-    public Aluno converterParaAluno() {
-        return new Aluno(nome, cpf, emprestimosAtivos, matricula);
-    }
-
-    @Override
-    public Usuario converterParaUsuario() {
-        return converterParaAluno();
-    }
-
-    public static EntidadeAluno converterParaEntidade(Aluno aluno) {
-        return new EntidadeAluno(
-                aluno.getNome(),
-                aluno.getCpf(),
-                aluno.getEmprestimosAtivos(),
-                aluno.getMatricula());
-    }
 }
+
