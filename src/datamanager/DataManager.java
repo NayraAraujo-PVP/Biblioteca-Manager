56ece271c81636a1f300424627c2bf66e678f20c
leticialopes1-gif <leticia.lopes1@estudante.ufla.br>
2026-06-11 17:10:09 -0300

    comentarios DM
    Added documentation for DataManager interface methods.

--- src/datamanager/DataManager.java
+++ src/datamanager/DataManager.java
@@ -3,7 +3,27 @@ package datamanager;
 import java.util.Collection;
 import java.util.List;
 
+/**
+ * Interface responsável pela persistência e recuperação de dados.
+ */
 public interface DataManager {
+
+    /**
+     * Busca os dados armazenados em um arquivo.
+     *
+     * @param fileName nome do arquivo.
+     * @param tClass tipo dos objetos armazenados.
+     * @param <T> tipo dos dados.
+     * @return lista de objetos recuperados.
+     */
     <T> List<T> buscar(String fileName, Class<T> tClass);
+
+    /**
+     * Salva uma coleção de dados em um arquivo.
+     *
+     * @param fileName nome do arquivo.
+     * @param data dados a serem armazenados.
+     * @param <T> tipo dos dados.
+     */
     <T> void salvar(String fileName, Collection<T> data);
 }
