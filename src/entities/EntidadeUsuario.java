84257266410e9f74b8b605a22addb4f8ffd659ba
leticialopes1-gif <leticia.lopes1@estudante.ufla.br>
2026-06-11 17:51:43 -0300

    Enhance EntidadeUsuario with Javadoc comments
    Added documentation comments for the EntidadeUsuario class and its methods.

--- src/entities/EntidadeUsuario.java
+++ src/entities/EntidadeUsuario.java
@@ -2,6 +2,66 @@ package entities;
 
 import domain.Usuario;
 
+/**
+ * Representa a entidade base de persistência para usuários.
+ *
+ * Armazena os dados comuns entre alunos e docentes.
+ */
+public abstract class EntidadeUsuario {
+
+    protected String nome;
+    protected String cpf;
+    protected int emprestimosAtivos;
+
+    /**
+     * Construtor vazio utilizado na desserialização.
+     */
+    protected EntidadeUsuario() {}
+
+    /**
+     * Cria uma entidade de usuário.
+     *
+     * @param nome nome do usuário.
+     * @param cpf CPF do usuário.
+     * @param emprestimosAtivos quantidade de empréstimos ativos.
+     */
+    protected EntidadeUsuario(String nome, String cpf, int emprestimosAtivos) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.emprestimosAtivos = emprestimosAtivos;
+    }
+
+    /**
+     * @return CPF do usuário.
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @return nome do usuário.
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @return quantidade de empréstimos ativos.
+     */
+    public int getEmprestimosAtivos() {
+        return emprestimosAtivos;
+    }
+
+    /**
+     * Converte a entidade para um objeto do domínio.
+     *
+     * @return usuário correspondente.
+     */
+    public abstract Usuario converterParaUsuario();
+}package entities;
+
+import domain.Usuario;
+
 public abstract class EntidadeUsuario {
     protected String nome;
     protected String cpf;
