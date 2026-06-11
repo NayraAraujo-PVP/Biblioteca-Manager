d1e8128fa4b78210f500448f62f506f8fc15cb04
leticialopes1-gif <leticia.lopes1@estudante.ufla.br>
2026-06-11 17:54:56 -0300

    Update EntidadeUsuario.java
    Removed the implementation of the EntidadeUsuario class.

--- src/entities/EntidadeUsuario.java
+++ src/entities/EntidadeUsuario.java
@@ -58,34 +58,4 @@ public abstract class EntidadeUsuario {
      * @return usuário correspondente.
      */
     public abstract Usuario converterParaUsuario();
-}package entities;
-
-import domain.Usuario;
-
-public abstract class EntidadeUsuario {
-    protected String nome;
-    protected String cpf;
-    protected int emprestimosAtivos;
-
-    protected EntidadeUsuario() {}
-
-    protected EntidadeUsuario(String nome, String cpf, int emprestimosAtivos) {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.emprestimosAtivos = emprestimosAtivos;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public int getEmprestimosAtivos() {
-        return emprestimosAtivos;
-    }
-
-    public abstract Usuario converterParaUsuario();
 }
