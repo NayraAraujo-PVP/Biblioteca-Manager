1b5191724af59132036880c983b1834a7db8179e
leticialopes1-gif <leticia.lopes1@estudante.ufla.br>
2026-06-11 17:13:26 -0300

    comentarios TA
    Added documentation comments for academic titles.

--- src/domain/enums/TitulacaoAcademica.java
+++ src/domain/enums/TitulacaoAcademica.java
@@ -1,7 +1,16 @@
 package domain.enums;
 
+/**
+ * Representa as titulações acadêmicas de um docente.
+ */
 public enum TitulacaoAcademica {
+
+    /** Titulação de mestre. */
     MESTRE,
+
+    /** Titulação de doutor. */
     DOUTOR,
+
+    /** Titulação de especialista. */
     ESPECIALISTA
 }
