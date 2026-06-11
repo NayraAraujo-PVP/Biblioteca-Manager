bedd4582687099452e0f27821826d82b029e89b0
leticialopes1-gif <leticia.lopes1@estudante.ufla.br>
2026-06-11 17:34:23 -0300

    Enhance EntidadeAluno with conversion methods
    Added methods for converting between Aluno and EntidadeAluno.

--- src/entities/EntidadeAluno.java
+++ src/entities/EntidadeAluno.java
@@ -73,5 +73,80 @@ public class EntidadeAluno extends EntidadeUsuario {
                 aluno.getMatricula()
         );
     }
+}package entities;
+
+import domain.Aluno;
+import domain.Usuario;
+
+/**
+ * Representa a entidade de persistência de um aluno.
+ *
+ * Utilizada para armazenar e recuperar dados de alunos
+ * nos arquivos da aplicação.
+ */
+public class EntidadeAluno extends EntidadeUsuario {
+
+    private String matricula;
+
+    /**
+     * Construtor vazio utilizado na desserialização.
+     */
+    public EntidadeAluno() {}
+
+    /**
+     * Cria uma entidade de aluno.
+     *
+     * @param nome nome do aluno.
+     * @param cpf CPF do aluno.
+     * @param emprestimosAtivos quantidade de empréstimos ativos.
+     * @param matricula matrícula do aluno.
+     */
+    protected EntidadeAluno(String nome, String cpf,
+                            int emprestimosAtivos,
+                            String matricula) {
+        super(nome, cpf, emprestimosAtivos);
+        this.matricula = matricula;
+    }
+
+    /**
+     * @return matrícula do aluno.
+     */
+    public String getMatricula() {
+        return matricula;
+    }
+
+    /**
+     * Converte a entidade para um objeto Aluno.
+     *
+     * @return objeto do tipo Aluno.
+     */
+    public Aluno converterParaAluno() {
+        return new Aluno(nome, cpf, emprestimosAtivos, matricula);
+    }
+
+    /**
+     * Converte a entidade para um usuário.
+     *
+     * @return usuário correspondente.
+     */
+    @Override
+    public Usuario converterParaUsuario() {
+        return converterParaAluno();
+    }
+
+    /**
+     * Converte um aluno para sua entidade de persistência.
+     *
+     * @param aluno aluno a ser convertido.
+     * @return entidade correspondente.
+     */
+    public static EntidadeAluno converterParaEntidade(Aluno aluno) {
+        return new EntidadeAluno(
+                aluno.getNome(),
+                aluno.getCpf(),
+                aluno.getEmprestimosAtivos(),
+                aluno.getMatricula()
+        );
+    }
 }
 
